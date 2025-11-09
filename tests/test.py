eba8605fffc1ffcde61a19ed5c173490b42b0286
RickytheGuy <rickythepsguy@gmail.com>
2025-11-08 20:30:53 -0700

    try multiple times

--- tests/test.py
+++ tests/test.py
@@ -15,32 +15,39 @@ if __name__ == "__main__":
     landcover_directory = r"C:\Users\lrr43\Documents\lu"
     oceans_pq = r"C:\Users\lrr43\Documents\worldmaps\seas_buffered.parquet"
     streamlines_directory = r"C:\Users\lrr43\Documents\worldmaps\streamlines"
-
-    (
-        FloodManager(
-        dem_dirs=dem_dirs,
-        dem_names=dem_names,
-        output_dir=output_dir,
-        landcover_directory=landcover_directory,
-        streamlines_directory=streamlines_directory,
-        oceans_pq=oceans_pq,
-        # bbox=bbox,
-        rps=[2, 5, 10, 25, 50, 100],
-        number_of_tiles=2700,
-        offset=0,
-        # overwrite_majority_maps=True,
-        # overwrite_floodmaps=True,
-        # overwrite_burned_dems=True,
-        # overwrite_vdts=True,
-        # overwrite_buffered_dems=True,
-        # overwrite_landuse=True,
-        # overwrite_streams=True,
-        )
-        .download_tilezen(r"C:\Users\lrr43\Documents\tilezen")
-        .download_alos(r"C:\Users\lrr43\Documents\alos_dems")
-        .download_fabdem(r"C:\Users\lrr43\Documents\fabdems")
-        .download_landcover()
-        .download_streamlines()
-        .download_oceans_pq(oceans_pq)
-        .run_all()
-    )
+    tries = 0
+    while tries < 20:
+        try:
+            (
+                FloodManager(
+                dem_dirs=dem_dirs,
+                dem_names=dem_names,
+                output_dir=output_dir,
+                landcover_directory=landcover_directory,
+                streamlines_directory=streamlines_directory,
+                oceans_pq=oceans_pq,
+                # bbox=bbox,
+                rps=[2, 5, 10, 25, 50, 100],
+                number_of_tiles=2700,
+                offset=0,
+                # overwrite_majority_maps=True,
+                # overwrite_floodmaps=True,
+                # overwrite_burned_dems=True,
+                # overwrite_vdts=True,
+                # overwrite_buffered_dems=True,
+                # overwrite_landuse=True,
+                # overwrite_streams=True,
+                )
+                .download_tilezen(r"C:\Users\lrr43\Documents\tilezen")
+                .download_alos(r"C:\Users\lrr43\Documents\alos_dems")
+                .download_fabdem(r"C:\Users\lrr43\Documents\fabdems")
+                .download_landcover()
+                .download_streamlines()
+                .download_oceans_pq(oceans_pq)
+                .run_all()
+            )
+            break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            tries += 1
+            print(f"Retrying... Attempt {tries}/20")
